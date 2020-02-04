3f24e2656441f178a86e4470f3e427d316312380
milenoss <milenkhanal@gmail.com>
2020-02-04 00:07:22 +0000

    stripe integration and fav icon; ;

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -4,6 +4,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCartItems} from '../../redux/cart/cart.selector'
 import {selectCartTotal} from '../../redux/cart/cart.selector'
 import CheckoutItem from '../../components/checkout-item/checkout-item'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 import './checkout.styles.scss'
 
@@ -35,7 +36,13 @@ const CheckoutPage = ({cartItems, total }) => (
     }
 
     <div className='total'>
-     <span>TOTAL: ${total}</span>
+    <div className = 'test-warning'>
+    *Please use the following test credit card for payments*
+    <br/>
+     4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+
+    </div>
+    <StripeCheckoutButton price={total} />
     </div>
 
     </div>
